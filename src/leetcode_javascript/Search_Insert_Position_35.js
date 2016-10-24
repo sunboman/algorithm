1fe61d630a69a4ad7866972f75edb62855e7ce7b
snowan <snowan@foxmail.com>
2016-10-25 03:27:39 +0800

    add 35

---
+++ src/leetcode_javascript/Search_Insert_Position_35.js
@@ -0,0 +1,41 @@
+/**
+ * 35 Search Insert Position
+ *
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+  You may assume no duplicates in the array.
+
+  Here are few examples.
+  [1,3,5,6], 5 → 2
+  [1,3,5,6], 2 → 1
+  [1,3,5,6], 7 → 4
+  [1,3,5,6], 0 → 0
+ *
+ *
+ * https://leetcode.com/problems/search-insert-position/
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if (nums.length === 0) {
+        return 0;
+    }
+    var lo = 0,
+        hi = nums.length - 1,
+        mid = 0;
+    while (lo <= hi) {
+        mid = lo + Math.floor((hi - lo) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    return lo;
+};
