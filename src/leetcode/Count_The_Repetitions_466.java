c5f6f4dcefcfadff103803c6417736db7b769ac6
Bo Sun <sunboman@gmail.com>
2017-04-26 18:40:06 -0400

    466

--- src/leetcode/Count_The_Repetitions_466.java
+++ src/leetcode/Count_The_Repetitions_466.java
@@ -5,24 +5,28 @@ package leetcode;
  */
 public class Count_The_Repetitions_466 {
   public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
-    int n = s1.length(), m = s2.length();
-    int count1 = 1, count2 = 0, i = 0, j = 0;
-    while (count1 <= n1) {
-      if (s1.charAt(i) == s2.charAt(j)) {
-        if (++j == m) {
+    char[] array1 = s1.toCharArray(), array2 = s2.toCharArray();
+    int count1 = 0, count2 = 0, i = 0, j = 0;
+
+    while (count1 < n1) {
+      if (array1[i] == array2[j]) {
+        j++;
+        if (j == array2.length) {
           j = 0;
           count2++;
         }
       }
-      if (++i == n) {
+      i++;
+      if (i == array1.length) {
         i = 0;
         count1++;
       }
     }
+
     return count2 / n2;
   }
 
   public static void main(String[] args) {
-    new Count_The_Repetitions_466().getMaxRepetitions("acb", 4, "ab", 2);
+    new Count_The_Repetitions_466().getMaxRepetitions("caahumeayllfdxfircvscxggbwkfnqduxwfnfozvsrtkjprepggxrpnrvystmwcysyycqpevikeffmznimkkasvwsrenazkycxaa", 1000000, "aac", 100);
   }
 }
